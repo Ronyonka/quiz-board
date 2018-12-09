f55501c2aaba144fe1368c3f716d6dcd1547571a
Ronyonka <rononyonka@gmail.com>
2018-12-09 23:12:11 +0300

    added jquery file to  js folder

--- js/script.js
+++ js/script.js
@@ -1,22 +1,19 @@
-// Business Logic
-function quiz(result){
-  var total = 0;
-  var choices = ["q1", "q2", "q3"];
-
-  choices.forEach(function(choice) {
-        var result = $("input:radio[name=" + choice + "]:checked").value();
-        if (result === "1") {
-          total += 20;
-        }
-      );
-}
-// User Logic
-$(document).ready(function(){
-$("#button1").click(function(event){
-  event.preventDefault();
-  $("#question2").show();
-});
-$("#finalScore").text(function(){
-  
-});
-});
+// // Business Logic
+// function quiz(result){
+//   var total = 0;
+//   var choices = ["q1", "q2", "q3"];
+//
+//   choices.forEach(function(choice) {
+//         var result = $("input:radio[name=" + choice + "]:checked").value();
+//         if (result === "1") {
+//           total += 20;
+//         }
+//       );
+// }
+// // User Logic
+// $(document).ready(function(){
+// $("#button1").click(function(){
+//   $("#question2").slideDown();
+// });
+// });
+alert("Hello World!");
