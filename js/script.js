7acd0782818a3409b76448fe4e2af159cec50bb0
Ronyonka <rononyonka@gmail.com>
2018-12-10 11:36:49 +0300

    added styling

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 // Business logic
 function quizBoard() {
   var qs1 = $("input:radio[name=q1]:checked").val();
-  var qs2 = $("input:radio[name=q1]:checked").val();
-  var qs3 = $("input:radio[name=q1]:checked").val();
-  var qs4 = $("input:radio[name=q1]:checked").val();
-  var qs5 = $("input:radio[name=q1]:checked").val();
+  var qs2 = $("input:radio[name=q2]:checked").val();
+  var qs3 = $("input:radio[name=q3]:checked").val();
+  var qs4 = $("input:radio[name=q4]:checked").val();
 
-  var finalScore = parseInt(qs1) + parseInt(qs2) + parseInt(qs3) + parseInt(qs4) + parseInt(qs5);
+
+  var finalScore = parseInt(qs1) + parseInt(qs2) + parseInt(qs3) + parseInt(qs4);
   $("#result").text("Your score is " + finalScore + "%");
 }
+
+// User logic
