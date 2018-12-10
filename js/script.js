7cf23b2f1bdcd00699a2292421f84ad67f7e102a
Ronyonka <rononyonka@gmail.com>
2018-12-10 10:27:09 +0300

    added business logic and styling

--- js/script.js
+++ js/script.js
@@ -1,22 +1,11 @@
-// Business Logic
-function quiz(){
-  var total = 0;
-//   var choices = ["q1", "q2", "q3", "q4", "q5"];
-//
-//   choices.forEach(function(choice) {
-//         var result = $("input:radio[name=" + choice + "]:checked").val();
-// }
-var qs1 = $("input:radio[name="q1"]:checked").val();
-var qs2 = $("input:radio[name="q2"]:checked").val();
-var qs3 = $("input:radio[name="q3"]:checked").val();
-var qs4 = $("input:radio[name="q4"]:checked").val();
-var qs5 = $("input:radio[name="q5"]:checked").val();
+// Business logic
+function quizBoard() {
+  var qs1 = $("input:radio[name=q1]:checked").val();
+  var qs2 = $("input:radio[name=q1]:checked").val();
+  var qs3 = $("input:radio[name=q1]:checked").val();
+  var qs4 = $("input:radio[name=q1]:checked").val();
+  var qs5 = $("input:radio[name=q1]:checked").val();
 
-var finalScore = parseInt("qs1") + parseInt("qs2") + parseInt("qs3") + parseInt("qs4") + parseInt("qs5")
-alert('finalScore');
-// var quizResult = $("#finalScore");
-// quizResult.innerHTML = total
-// User Logic
-// $(document).ready(function(){
-//
-// });
+  var finalScore = parseInt(qs1) + parseInt(qs2) + parseInt(qs3) + parseInt(qs4) + parseInt(qs5);
+  $("#result").text("Your score is " + finalScore + "%");
+}
